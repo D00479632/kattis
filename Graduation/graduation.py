1994b816e7f0802632a077a256c80b4a6627b207
Paula Lozano Gonzalo <paulalozanogonzalo@Paulas-Air.lan>
2024-07-06 17:25:04 +0200

    Need to solve the problem

---
+++ Graduation/graduation.py
@@ -0,0 +1,11 @@
+# N - Rows
+# M - Columns
+# K - Classes
+
+# Print an integer – the maximum number of unique hat colors that can be 
+# assigned to the students so that all the students in the same column have the same hat color,
+# and also all students in the same class have the same hat color.
+
+import sys
+
+data = sys.stdin
